5ac9a621635211e570e6213b111365fac8c633e8
Jonny Baho <jonnybaho@event-10-102-28-200.venue.apple.com>
2015-06-09 10:39:51 -0700

    initial commit

---
+++ WikiGame/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  WikiGame
+//
+//  Created by Jonny Baho on 08/06/15.
+//  Copyright (c) 2015 Jonny Baho. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var extractLabel: UILabel!
+    @IBOutlet weak var guessTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let unparsedString = "{'query':{'pages':{'3747':{'pageid':3747,'ns':0,'title':'Bill Gates','extract':'William Henry 'Bill' Gates III (born October 28, 1955) is an American business magnate, philanthropist, investor, computer programmer, and inventor. Gates originally established his reputation as the co-founder of Microsoft, the world largest PC software company, with Paul Allen. During his career at Microsoft, Gates held the positions of chairman, CEO and chief software architect, and was also the largest individual shareholder until May 2014. He has also authored and co-authored several books.\nToday he is consist'"
+        
+        /*
+        (?<=://) means preceded by ://
+        [^.]+    means any characters except .
+        (?=.)    means followed by .
+        */
+        
+        let range = unparsedString.rangeOfString("(?<=extract':)[^\n]+(?=)", options:.RegularExpressionSearch)
+       
+        if range != nil{
+            
+            let parsedString = unparsedString.substringWithRange(range!)
+            extractLabel.text = parsedString
+            
+        }
+        
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func guessButton(sender: AnyObject) {
+        
+        
+        
+    }
+
+}
+
