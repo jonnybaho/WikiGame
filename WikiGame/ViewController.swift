42280cea58ca8edc77301e8cda83ca0017325b81
Jonny Baho <jonnybaho@event-10-102-28-200.venue.apple.com>
2015-06-10 11:04:48 -0700

    stuff

--- WikiGame/ViewController.swift
+++ WikiGame/ViewController.swift
@@ -14,11 +14,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var extractLabel: UILabel!
     @IBOutlet weak var guessTextField: UITextField!
     @IBOutlet weak var titleLabel: UILabel!
+    var titleString = ""
+    @IBOutlet weak var labelView: UIView!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //TODO: Get the JSON and parse it
         
         let unparsedString = "{'query':{'pages':{'3747':{'pageid':3747,'ns':0,'title':'Bill Gates','extract':'William Henry 'Bill' Gates III (born October 28, 1955) is an American business magnate, philanthropist, investor, computer programmer, and inventor. Gates originally established his reputation as the co-founder of Microsoft, the world largest PC software company, with Paul Allen. During his career at Microsoft, Gates held the positions of chairman, CEO and chief software architect, and was also the largest individual shareholder until May 2014. He has also authored and co-authored several books.\nToday he is consist'"
         
@@ -33,7 +37,7 @@ class ViewController: UIViewController {
         
         if (range != nil && titleRange != nil){
             
-            let titleString = unparsedString.substringWithRange(titleRange!)
+            titleString = unparsedString.substringWithRange(titleRange!).uppercaseString
             let titleStringArr = titleString.componentsSeparatedByString(" ")
             var parsedString = unparsedString.substringWithRange(range!)
             
@@ -45,21 +49,19 @@ class ViewController: UIViewController {
             
             extractLabel.text = parsedString
 
-            do {
+            
+            var ierror: NSError?
+            let regex:NSRegularExpression =  NSRegularExpression(pattern: "[A-Z]", options: NSRegularExpressionOptions.CaseInsensitive, error: &ierror)!
+            
+            let modifiedString = regex.stringByReplacingMatchesInString(titleString, options:NSMatchingOptions.WithTransparentBounds, range: NSMakeRange(0, count(titleString)), withTemplate: "X")
                 
-                let regex:NSRegularExpression = try NSRegularExpression(pattern: "[A-Z]", options: NSRegularExpressionOptions.CaseInsensitive)
-                
-                let modifiedString = regex.stringByReplacingMatchesInString(titleString, options:NSMatchingOptions.WithTransparentBounds, range: NSMakeRange(0, titleString.characters.count), withTemplate: "X")
-                
-                titleLabel.text = modifiedString
-
-            } catch {
-                
-                print("Oh no!")
-                
-            }
+            titleLabel.text = modifiedString
             
         }
+        
+        showSomeLetters()
+        
+        labelView.layer.cornerRadius = 10
         
     }
     
@@ -71,8 +73,61 @@ class ViewController: UIViewController {
     
     @IBAction func guessButton(sender: AnyObject) {
         
+        if (guessTextField.text.lowercaseString == titleString.lowercaseString ){
+            
+            titleLabel.text = titleString + " - You were right!"
+            
+        }else {
+            
+            titleLabel.text = titleLabel.text! + " - You were wrong!"
+            
+            UIView.animateWithDuration(1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0, options: nil, animations: { () -> Void in
+                
+                    // Bouncing animation? To indicate the wrong answer
+                
+                }, completion: nil)
+            
+        }
         
     }
     
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        self.view.endEditing(true)
+        return false
+        
+    }
+    
+    @IBAction func tappedView(sender: AnyObject) {
+        
+        textFieldShouldReturn(guessTextField)
+        
+    }
+    
+    func showSomeLetters(){
+        
+        let amount = UInt32(count(titleString))
+        let randomInt = Int(arc4random_uniform(amount))
+        
+        let indexesToReplace = Set
+        
+        let charArray = Array(titleString)
+        
+        if (charArray[randomInt] != " "){
+            
+            var firstChar = charArray[randomInt]
+            Array(arrayLiteral: titleLabel.text)[randomInt] = firstChar
+            
+            // Replace X with the character
+            
+            ///ewgiuhewgohewgoewijgewoij4wy4wy
+        }else {
+            
+            showSomeLetters()
+            // Bad code: refreshes the function if the random int lands on a spacebar
+            
+        }
+        
+    }
 }
 
