97863c1bf5c7247db32c7217387d53f8ab17130e
Niket Shah <masterniket@gmail.com>
2015-06-10 16:07:54 -0700

    add random page support v1

--- WikiGame/ViewController.swift
+++ WikiGame/ViewController.swift
@@ -23,7 +23,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         //TODO: Get the JSON and parse it
-        
+		
+        GameProvider.getGameObject()
         let unparsedString = "{'query':{'pages':{'3747':{'pageid':3747,'ns':0,'title':'Bill Gates','extract':'William Henry 'Bill' Gates III (born October 28, 1955) is an American business magnate, philanthropist, investor, computer programmer, and inventor. Gates originally established his reputation as the co-founder of Microsoft, the world largest PC software company, with Paul Allen. During his career at Microsoft, Gates held the positions of chairman, CEO and chief software architect, and was also the largest individual shareholder until May 2014. He has also authored and co-authored several books.\nToday he is consist'"
         
         /*
