25a2ea654f717f51be3629382f6663f3ee4b5eca
Niket Shah <masterniket@gmail.com>
2015-06-11 16:29:41 -0700

    processGuess in playground

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -17,3 +17,17 @@ func replaceAllOccurencesInString(string: String, originalString: String, charac
 }
 
 replaceAllOccurencesInString("XXXXX", "STEVE", "E")
+
+
+func processGuess(string: String, originalString: String, guess:Character) -> String {
+	let characters = Array(originalString)
+	var characters2 = Array(string)
+	let result = ""
+	
+	for (var i = 0; i < characters.count; i++) {
+		characters2[i] = characters[i] == guess ? guess : characters2[i]
+	}
+	return String(characters2)
+}
+
+processGuess("SXXXX", "STEVE", "E")
